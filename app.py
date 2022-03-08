888662b9633c8ded2d372391c6d56abc90f2ef05
Patrick O'Brien <obrienpja@gmail.com>
2022-03-08 10:02:17 -0500

    Comment out most of simulate for testing

--- app.py
+++ app.py
@@ -144,7 +144,11 @@ def _df_cmat_to_adj_list(df_cmat: pd.DataFrame):
 
 
 def deserialize_tuple_dict_list(serialized_list):
-    return {literal_eval(k): v for k, v in serialized_list.items()}
+    print('serialized_list:', serialized_list)
+    print('items:', serialized_list.items())
+    data = {literal_eval(k): v for k, v in serialized_list.items()}
+    print(data)
+    return data
 
 
 def convert_netlist_to_maxwell(df):
@@ -157,44 +161,48 @@ def convert_netlist_to_maxwell(df):
 
 @app.route('/simulate', methods=['POST'])
 def simulate():
-    adjacency_list = json.loads(request.json['adjacency_list'])
-    node_rename_list = json.loads(request.json['node_rename_list'])
-    ind_dict_list = json.loads(request.json['ind_dict_list'])
-    jj_dict_list = json.loads(request.json['jj_dict_list'])
-    cj_dict_list = json.loads(request.json['cj_dict_list'])
-    subsystem_list = json.loads(request.json['subsystem_list'])
+    req = request.get_json()
+    print("Circuit Graph:", req["CircuitGraph"])
+    # adjacency_list = json.loads(req['adjacencyList'])
+    # node_rename_list = json.loads(req['nodeRenameList'])
+    # ind_dict_list = req['indDictList']
+    # jj_dict_list = json.loads(req['jjDictList'])
+    # cj_dict_list = json.loads(req['cjDictList'])
+    #
+    # subsystem_list = json.loads(req['subsystemList'])
+    #
+    # c_mats = []
+    # for adjacency in adjacency_list:
+    #     nodes = adjacency.keys()
+    #     inp_keys_index = pd.Index(nodes)
+    #     c_mats.append(_make_cmat_df(adj_list_to_mat(inp_keys_index, adjacency), nodes))
+    #
+    # cell_list = []
+    # for ii in range(len(node_rename_list)):
+    #     print(c_mats[ii])
+    #     converted = convert_netlist_to_maxwell(c_mats[ii])
+    #     print(converted)
+    #     cell_list.append(Cell(dict(node_rename=node_rename_list[ii],
+    #                           cap_mat=converted,
+    #                           ind_dict=deserialize_tuple_dict_list(json.loads(ind_dict_list[ii])),
+    #                           jj_dict=deserialize_tuple_dict_list(jj_dict_list[ii]),
+    #                           cj_dict=deserialize_tuple_dict_list(cj_dict_list[ii]))))
+    #
+    # subsystems = []
+    # for subsystem in subsystem_list:
+    #     subsystems.append(Subsystem(name=subsystem['name'], sys_type=subsystem['sys_type'],
+    #                                 nodes=subsystem['nodes'], q_opts=subsystem.get('q_opts', None)))
+    #
+    # composite_sys = CompositeSystem(
+    #     subsystems=subsystems,
+    #     cells=cell_list,
+    #     grd_node='ground_main_plane',
+    #     nodes_force_keep=['readout_alice', 'readout_bob']
+    # )
+    #
+    # hilbertspace = composite_sys.add_interaction()
+    # hamiltonian_results = composite_sys.hamiltonian_results(hilbertspace, evals_count=30)
+    # hamiltonian_results['chi_in_MHz'] = hamiltonian_results['chi_in_MHz'].to_dataframe().to_dict()
 
-    c_mats = []
-    for adjacency in adjacency_list:
-        nodes = adjacency.keys()
-        inp_keys_index = pd.Index(nodes)
-        c_mats.append(_make_cmat_df(adj_list_to_mat(inp_keys_index, adjacency), nodes))
-
-    cell_list = []
-    for ii in range(len(node_rename_list)):
-        print(c_mats[ii])
-        converted = convert_netlist_to_maxwell(c_mats[ii])
-        print(converted)
-        cell_list.append(Cell(dict(node_rename=node_rename_list[ii],
-                              cap_mat=converted,
-                              ind_dict=deserialize_tuple_dict_list(json.loads(ind_dict_list[ii])),
-                              jj_dict=deserialize_tuple_dict_list(json.loads(jj_dict_list[ii])),
-                              cj_dict=deserialize_tuple_dict_list(json.loads(cj_dict_list[ii])))))
-
-    subsystems = []
-    for subsystem in subsystem_list:
-        subsystems.append(Subsystem(name=subsystem['name'], sys_type=subsystem['sys_type'],
-                                    nodes=subsystem['nodes'], q_opts=subsystem.get('q_opts', None)))
-
-    composite_sys = CompositeSystem(
-        subsystems=subsystems,
-        cells=cell_list,
-        grd_node='ground_main_plane',
-        nodes_force_keep=['readout_alice', 'readout_bob']
-    )
-
-    hilbertspace = composite_sys.add_interaction()
-    hamiltonian_results = composite_sys.hamiltonian_results(hilbertspace, evals_count=30)
-    hamiltonian_results['chi_in_MHz'] = hamiltonian_results['chi_in_MHz'].to_dataframe().to_dict()
-
-    return hamiltonian_results
+    # return hamiltonian_results
+    return '0'
