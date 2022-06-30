2dc1c035a320374ae6efbd3f7c6dcbf60b831323
Connor Bowman <connorbowman4@yahoo.com>
2022-06-30 13:38:02 -0400

    Removes comment :

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from flask import Flask
 from flask_sock import Sock
 
-from simulation import simulate #rename simulate to lom_simulate?
+from simulation import simulate  #rename simulate to lom_simulate?
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -26,6 +26,3 @@ def sim(sock):
 
 
 CORS(app)
-
-# pull in cahnges from connor-main for backend (this)
-# pull in changes from curent-main for frontend
